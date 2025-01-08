63536a2d7e076409de8f268caf65b8138d043198
kun.d.jung@selvas.com <kun.d.jung@selvas.com>
2025-01-08 23:34:51 +0900

    readme 작성

--- server/order/src/routes/api/index.js
+++ server/order/src/routes/api/index.js
@@ -3,9 +3,10 @@ const orderController = require('./orderController');
 
 const router = express.Router();
 
-// 주문 데이터 처리 API
-router.post('/orders/', orderController.createOrder); // 외부 주문 데이터 수신
-// 특정 외부 시스템에서 주문 데이터 수집
+// 주문 데이터 생성 (For TEST)
+router.post('/orders/', orderController.createOrder);
+
+// 특정 외부 시스템에서 주문 데이터 가져오기
 router.post('/orders/fetch', orderController.fetchExternalOrders);
 router.get('/orders/', orderController.readAllOrders); // 주문 리스트 조회
 router.get('/orders/:id', orderController.readOrderById); // 주문 ID로 조회
